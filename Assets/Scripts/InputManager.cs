6f157ccef9d4985064ff88fd8764e90797d1a79e
ssarker <432-ssarker@users.noreply.gitlab1.its.sfu.ca>
2020-05-16 00:07:24 -0700

    Place an Object to the World

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] private GameObject arObj;
+    [SerializeField] private Camera arCam;
+    [SerializeField] private ARRaycastManager _raycastManager;
+
+    private List<ARRaycastHit> _hits = new List<ARRaycastHit>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = arCam.ScreenPointToRay(Input.mousePosition);
+            if (_raycastManager.Raycast(ray,_hits))
+            {
+                Pose pose = _hits[0].pose;
+                Instantiate(arObj, pose.position, pose.rotation);
+            }        
+            
+        }
+    }
+}
