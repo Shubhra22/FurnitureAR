73655ccb79fc3ca6fa35a80f357c9733fba6aec6
ssarker <432-ssarker@users.noreply.gitlab1.its.sfu.ca>
2020-05-16 17:34:13 -0700

    Basic controls to select model from UI

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    private Button btn;
+
+    public GameObject furniture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        btn = GetComponent<Button>();
+        btn.onClick.AddListener(SelectObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SelectObject()
+    {
+        DataHandler.Instance.furniture = furniture;
+    }
+}
