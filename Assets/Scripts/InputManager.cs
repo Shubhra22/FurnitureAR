145757c87f9aa300317a8b9f0ab238b6405cc340
Shubhra Sarker <ssuvro22@gmail.com>
2020-05-23 23:14:05 -0700

    Crosshair interaction

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -8,40 +8,56 @@ public class InputManager : MonoBehaviour
 {
     [SerializeField] private Camera arCam;
     [SerializeField] private ARRaycastManager _raycastManager;
-
+    [SerializeField] private GameObject crosshair;
+    
     private List<ARRaycastHit> _hits = new List<ARRaycastHit>();
 
     private Touch touch;
+    private Pose pose;
+   
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+        CrosshairCalculation();
+        
         touch = Input.GetTouch(0);
 
         if (Input.touchCount < 0 || touch.phase != TouchPhase.Began)
             return;
 
+
         if (IsPointerOverUI(touch)) return;
-        
-        Ray ray = arCam.ScreenPointToRay(touch.position);
-        if (_raycastManager.Raycast(ray,_hits))
-        {
-           Pose pose = _hits[0].pose;
-           Instantiate(DataHandler.Instance.furniture, pose.position, pose.rotation);
-        }
+
+        Instantiate(DataHandler.Instance.furniture, pose.position, pose.rotation);
+
     }
 
     bool IsPointerOverUI(Touch touch)
     {
         PointerEventData eventData = new PointerEventData(EventSystem.current);
-        eventData.position = new Vector2(touch.position.x,touch.position.y);
+        eventData.position = new Vector2(touch.position.x, touch.position.y);
         List<RaycastResult> results = new List<RaycastResult>();
-        EventSystem.current.RaycastAll(eventData,results);
+        EventSystem.current.RaycastAll(eventData, results);
         return results.Count > 0;
     }
+
+    void CrosshairCalculation()
+    {
+        Vector3 origin = arCam.ViewportToScreenPoint(new Vector3(0.5f, 0.5f, 0));
+        Ray ray = arCam.ScreenPointToRay(origin);
+      
+        if (_raycastManager.Raycast(ray, _hits))
+        {
+            pose = _hits[0].pose;
+            crosshair.transform.position = pose.position;
+            crosshair.transform.eulerAngles = new Vector3(90,0,0);
+        }
+    }
+    
 }
